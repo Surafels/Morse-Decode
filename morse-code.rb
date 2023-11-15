8b3f3b0f1cbd552a7e33bc46ec55d077c19708eb
Surafels <ssurafel4@gmail.com>
2023-11-15 18:34:15 +0300

    Add decode word and message function

--- morse-code.rb
+++ morse-code.rb
@@ -31,4 +31,17 @@ def decode_char(morse_code)
   CODE_CHAR[morse_code] || ""
 end
  
+def decode_word(morse_word)
+  morse_word.split(" ").map{ |morse_char| decode_char(morse_char)}.join("")
+  end
 
+  def decode_message(morse_message)
+    morse_message.split(" ").map{ |morse_word| decode_word(morse_word)}.join("")
+  end
+
+  puts decode_char(".-") 
+  puts decode_word("-- -.--")
+
+  message = "-- -.--   -. .- -- ."
+  decoded_message = decode_message(message)
+  puts decoded_message
