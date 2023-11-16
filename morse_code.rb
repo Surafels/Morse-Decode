0070a6317c95adca47c98fe6c14baa2f4e21c9a5
unknown <ismail halliru muhammad>
2023-11-16 12:52:04 +0100

    split the message words

--- morse_code.rb
+++ morse_code.rb
@@ -1,48 +1,28 @@
 CODE_CHAR = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
-}.freeze
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
+}
 
 def decode_char(morse_code)
   CODE_CHAR[morse_code] || ''
 end
 
 def decode_word(morse_word)
-  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join('')
 end
 
 def decode_message(morse_message)
-  morse_message.split.map { |morse_word| decode_word(morse_word) }.join
+  morse_message.split(' / ').map { |morse_word| decode_word(morse_word) }.join(' ')
 end
 
-puts decode_char('.-')
-puts decode_word('-- -.--')
+puts decode_char('.-')         # Output: A
+puts decode_word('-- -.--')    # Output: MY
 
 message = '-- -.-- / -. .- -- .'
-
 decoded_message = decode_message(message)
-puts decoded_message
+puts decoded_message           # Output: MY NAME
